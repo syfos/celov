32a646f0413f30afc04d1c6193f9cf93ae1a9224
syfos <261843105+syfos@users.noreply.github.com>
2026-09-07 11:34:58 +0530

    refactor(unicode): remove uneeded imports from "mod.rs"

--- src/ui/unicode/mod.rs
+++ src/ui/unicode/mod.rs
@@ -1,15 +1,4 @@
-use std::ops;
-
-use icu_segmenter::GraphemeClusterSegmenter;
-use unicode_bidi::BidiInfo;
-use unicode_normalization::{is_nfc, is_nfd};
-use unicode_width::UnicodeWidthStr;
-
-use crate::ui::unicode::{bidi_order::BidiAwareLine, grapheme_boundary::GraphemeBoundary};
-
 pub mod bidi_order;
 pub mod grapheme_boundary;
 pub mod query_normalization;
 pub mod unicode_struct;
-
-
