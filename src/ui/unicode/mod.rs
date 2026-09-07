ae2c1f1df01ac55e3cd58ea6a084c70df85c274e
syfos <261843105+syfos@users.noreply.github.com>
2026-09-07 07:42:03 +0530

    fix(unicode): change "Unicode::into_grapheme_line" to "Unicode::get_grapheme_boundary" and use "icu_segmemter" instead of "unicode_segmenter" for the function

--- src/ui/unicode/mod.rs
+++ src/ui/unicode/mod.rs
@@ -1,6 +1,8 @@
+use std::{ops};
+
+use icu_segmenter::GraphemeClusterSegmenter;
 use unicode_bidi::BidiInfo;
 use unicode_normalization::{is_nfc, is_nfd};
-use unicode_segmentation::UnicodeSegmentation;
 use unicode_width::UnicodeWidthStr;
 
 pub enum CanonicalType {
@@ -18,7 +20,7 @@ pub enum CanonicalType {
 #[allow(dead_code)]
 pub struct Unicode {
   /// Viewport lines into Grapheme aware lines
-  pub viewport_grapheme_lines: Vec<Vec<Graphemes>>,
+  pub viewport_grapheme_lines: Vec<Vec<GraphemeBoundary>>,
   /// Viewport lines into Bidirection aware lines
   pub viewport_bidirectional_lines: Vec<BidiAwareLine>,
 }
@@ -33,47 +35,99 @@ pub struct BidiAwareLine {
 
 #[allow(dead_code)]
 #[derive(Default, Debug, Clone)]
-pub struct Graphemes {
-  /// The range of `Unicode` aware Grapheme between two absolute character indicies of a rope line.
-  pub rope_absolute_char_index_range: std::ops::Range<usize>,
+pub struct GraphemeBoundary {
+  pub line_idx: usize,
 
-  /// The width in `terminal cell` the Grapheme is occupying.
-  /// Used for cursor movement in terminal column.
-  pub term_cell_width: usize,
+  /// Tells width of current grapheme boundary in terms of `viewport's cells`.
+  /// Helps in movement of cursor.
+  /// ```
+  /// // Usage: 
+  /// let new_col_pos = cursor_col + next_grapheme.width;
+  /// let new_col_pos = cursor_col - prev_grapheme.width;
+  /// ```
+  pub width: usize,
 
-  // The cumulative width i.e sum of width of terminal cells occupied by very first `Grapheme` all the way to current `Grapheme`.
-  // The cumulative_term_cell_width of very first Grapheme is always equal to `term_cell_width` of the Grapheme.
-  // Derived by `cumulative_term_cell_width += term_cell_width` for each grapheme.
-  pub cumulative_term_cell_width: usize,
+  /// The cumulative width sum for the current grapheme boundary of the rope string.
+  /// Why exists?: To help [`Softwrap`] do the wrap math.
+  pub cumulative_width: usize,
+
+  /// The rope byte idx range the graphemes stands on.
+  /// E.g, say for a CJK string: [0..3, 3..6, 6..9, 9..12]
+  /// Your byte boundaries -> [0..=2, 3..=5, 6..=8, 9..=11]
+  pub absolute_byte_idx: ops::Range<usize>,
+}
+
+impl Unicode {
+  /// This function returns [`GraphemeBoundary`] containing useful data related to the rope line.
+  ///
+  /// Quick overview:
+  /// 1. Line index
+  /// 2.  Boundary width in viewport cells, for cursor movement. 
+  /// 3. Cumulative width of boundary, for [`Softwrap`] based softwrap.
+  /// 4. Absolute byte index range of the grapheme in the given rope line. 
+  ///
+  /// Why exists?:
+  /// Exists because you can't reliably edit/wrap a rope string without knowing the boundaries.
+  pub fn get_grapheme_boundary(
+    rope_line: &str,
+    line_idx: usize,
+    line_to_byte: usize,
+  ) -> Vec<GraphemeBoundary> {
+    // Todo: Move the segementer to a struct to avoid regeneration.
+    let segment = GraphemeClusterSegmenter::new();
+    // Generate vector containing byte indicies.
+    let breakpoints: Vec<usize> = segment.segment_str(rope_line).collect();
+
+    let mut cumulative_width = 0usize;
+
+    // Returns exclusive absolute byte index vector.
+    // For e.g: [0..3, 3..4, 4..6]
+    //
+    // Note: Since this default range excludes the end value, you just use it as:
+    // 0..=2, 3..=3, 4..=5 with respective counts as 3, 1, and 2
+    breakpoints
+      // Yeilds the slices/windows each of length 2.
+      // E.g: [0, 3, 6, 9, 12]
+      // 0..3, 3..6, 6..9, 9..12
+      .windows(2)
+      // byte_window is a slice/window of length 2
+      // line_to_byte + byte_idx -> generates absolute byte index for rope level operation.
+      .map(|byte_window| {
+        // Get the relative start and end byte index
+        // Why?: To get the grapheme slice.
+        let (rel_start, rel_end) = (byte_window[0], byte_window[1]);
+
+        // Todo: Do check is range exclusive even the right choice.
+        //
+        // Gives the grapheme slice from the rope string.
+        let grapheme_slice = &rope_line[rel_start..rel_end];
+
+        // Get width of current grapheme.
+        let width = grapheme_slice.width();
+
+        // Get the cumulative width 
+        // Why?: For grapheme aware soft wrapping.
+        cumulative_width += width;
+
+        // Get the absolute byte index.
+        // Why?: To work with rope's insert/remove api.
+        let absolute_byte_idx = (line_to_byte + byte_window[0])..(line_to_byte + byte_window[1]);
+
+        // Return 
+        GraphemeBoundary {
+          line_idx,
+          width,
+          cumulative_width,
+          absolute_byte_idx,
+        }
+      })
+      // collect and cast
+      .collect()
+  }
 }
 
 #[allow(dead_code)]
 impl Unicode {
-  /// Returns [`Vec<Graphemes>`] which contains all
-  /// `Graphemes` of given string along its `cell width` and its `cumulative_term_cell_width`.
-  /// Each [`Vec<Graphemes>`] is a Line.
-  /// The `rope_absolute_char_index_range` per `Grapheme` is character index range.
-  pub fn into_grapheme_line(line: &str, line_to_char: &usize) -> Vec<Graphemes> {
-    let mut boundary = Vec::new();
-    let mut offset = 0usize;
-    let mut cumulative_net_width = 0usize;
-    for grapheme in line.graphemes(true) {
-      let grapheme_net_chars = grapheme.chars().count();
-      let start = *line_to_char + offset;
-      offset += grapheme_net_chars;
-      let end = *line_to_char + offset;
-      let term_cell_width = grapheme.width();
-      cumulative_net_width += term_cell_width;
-      boundary.push(Graphemes {
-        rope_absolute_char_index_range: start..end,
-        term_cell_width,
-        cumulative_term_cell_width: cumulative_net_width,
-      });
-    }
-
-    boundary
-  }
-
   /// Flips chars of `RTL` language (e.g `Arabic`, `Persian`, `Hebrew`) words of given line into `RTL` logical sequence words keeping non RTL words intact.
   pub fn into_bidirectional_line(line: &str) -> BidiAwareLine {
     let bidi_info = BidiInfo::new(line, None);
