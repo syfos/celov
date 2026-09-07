1b3370369faa61ffed5515154b8fd81451c739aa
syfos <261843105+syfos@users.noreply.github.com>
2026-09-07 08:36:06 +0530

    docs(unicode): refine comments for "Unicode::into_bidirectional_line"

--- src/ui/unicode/mod.rs
+++ src/ui/unicode/mod.rs
@@ -1,4 +1,4 @@
-use std::{ops};
+use std::ops;
 
 use icu_segmenter::GraphemeClusterSegmenter;
 use unicode_bidi::BidiInfo;
@@ -41,7 +41,7 @@ pub struct GraphemeBoundary {
   /// Tells width of current grapheme boundary in terms of `viewport's cells`.
   /// Helps in movement of cursor.
   /// ```
-  /// // Usage: 
+  /// // Usage:
   /// let new_col_pos = cursor_col + next_grapheme.width;
   /// let new_col_pos = cursor_col - prev_grapheme.width;
   /// ```
@@ -62,9 +62,9 @@ impl Unicode {
   ///
   /// Quick overview:
   /// 1. Line index
-  /// 2.  Boundary width in viewport cells, for cursor movement. 
+  /// 2.  Boundary width in viewport cells, for cursor movement.
   /// 3. Cumulative width of boundary, for [`Softwrap`] based softwrap.
-  /// 4. Absolute byte index range of the grapheme in the given rope line. 
+  /// 4. Absolute byte index range of the grapheme in the given rope line.
   ///
   /// Why exists?:
   /// Exists because you can't reliably edit/wrap a rope string without knowing the boundaries.
@@ -105,7 +105,7 @@ impl Unicode {
         // Get width of current grapheme.
         let width = grapheme_slice.width();
 
-        // Get the cumulative width 
+        // Get the cumulative width
         // Why?: For grapheme aware soft wrapping.
         cumulative_width += width;
 
@@ -113,7 +113,7 @@ impl Unicode {
         // Why?: To work with rope's insert/remove api.
         let absolute_byte_idx = (line_to_byte + byte_window[0])..(line_to_byte + byte_window[1]);
 
-        // Return 
+        // Return
         GraphemeBoundary {
           line_idx,
           width,
@@ -128,27 +128,48 @@ impl Unicode {
 
 #[allow(dead_code)]
 impl Unicode {
-  /// Flips chars of `RTL` language (e.g `Arabic`, `Persian`, `Hebrew`) words of given line into `RTL` logical sequence words keeping non RTL words intact.
-  pub fn into_bidirectional_line(line: &str) -> BidiAwareLine {
-    let bidi_info = BidiInfo::new(line, None);
+  /// Flips the LTR character sequences in RTL words into RTL char sequence for correct logical and gramatical display.
+  pub fn into_bidirectional_line(rope_line: &str) -> BidiAwareLine {
+    // Generate bidirectional information about the rope line
+    let bidi_info = BidiInfo::new(rope_line, None);
 
-    // The internal UBA algorith works on Paragraphs.
-    // Hence if your line does contains any Paragraph
-    // seperator then this would consider it as
-    // (net_paragraph_seperator_count + 1)
+    // The internal UBA(Unicode Bidirectional Algorithm) algorith works on Paragraphs.
     //
-    // List of Paragraoh Seperator:
-    // [LF, CR, CRLF, NEL, LS, PS]
+    // Ropey crate considers paragraph seperators and other line break characters like '\n' equally.
+    //
+    // E.g of a rope line with paragraph seperator:
+    // "Hello, World!<U+2029>"
+    //
+    // Note: Despite a rope line being a single line with a recognized line termination character, but for the underlying algorithm it is still a single paragraph.
+    //
+    // Do give a visit to pub enum [`LineBreakChar`] for more information about list of all valid recognized line breaks by ropey.
     let paragraphs = &bidi_info.paragraphs[0];
 
-    // Odd number means RTL word
-    // Even number means LTR word
+    // Get the paragraph embedding level number.
+    // Odd number means rope line is RTL while,
+    // Even number means rope line is LTR
     let level_number = paragraphs.level.number();
+
+    // Check if is the line
     let is_rtl = paragraphs.level.is_rtl();
 
-    // Tells which part belong to what paragraph.
+    // Returns byte indices of paragraph boundary within the given rope line.
     let paragraph_range = paragraphs.range.clone();
 
+    // Reorder the RTL segments correctly.
+    // Note: 
+    // 1. This line is pure cosmetic display
+    // 2. The real character sequence will always be LTR beacuse computers don't really store RTL in RTL sequence.
+    // 3. The reordered line is purely ment for display purpose.
+    //
+    // E.g say you want to type : "שלום"
+    // But since Hebrew is RTL then
+    // You type: ["ם", "ו", "ל", "ש"]
+    // Computer stores: ["ם", "ו", "ל", "ש"]
+    // Computer displays: ["ש", "ל", "ו", "ם"]
+    //
+    // So here we are reordering the "unreadable LTR" sequence into "readable RTL" sequence i.e:
+    // Computer displays: ["ש", "ל", "ו", "ם"]
     let reordered_line = bidi_info
       .reorder_line(paragraphs, paragraph_range)
       .to_string();
