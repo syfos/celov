70fa20be809a35cfe4d944f09b5580353b2136d0
syfos <261843105+syfos@users.noreply.github.com>
2026-09-07 19:20:05 +0530

    feat(unicode): add pub struct "IcuEngines" to store Icu4x engines

---
+++ src/ui/unicode/icu_engines.rs
@@ -0,0 +1,21 @@
+use icu_segmenter::{
+  GraphemeClusterSegmenter, GraphemeClusterSegmenterBorrowed, WordSegmenter, WordSegmenterBorrowed,
+  options::WordBreakInvariantOptions,
+};
+
+/// Contains the heavier Icu4x engines that are expensive to compile more than once.
+/// Note: Caching any of the listed doesn't causes staleness because these are engines not some random methods.
+pub struct IcuEngines {
+  pub word: WordSegmenterBorrowed<'static>,
+  pub grapheme_cluster: GraphemeClusterSegmenterBorrowed<'static>,
+}
+
+impl IcuEngines {
+  /// Generate icu4x expensive engines.
+  pub fn new() -> Self {
+    Self {
+      word: WordSegmenter::new_auto(WordBreakInvariantOptions::default()),
+      grapheme_cluster: GraphemeClusterSegmenter::new(),
+    }
+  }
+}
--- src/ui/unicode/mod.rs
+++ src/ui/unicode/mod.rs
@@ -3,3 +3,4 @@ pub mod grapheme_boundary;
 pub mod query_normalization;
 pub mod unicode_struct;
 pub mod word_segmenter;
+pub mod icu_engines;
