9a7c037eccb7d8619f41c96274fff66fdf4f41b6
syfos <261843105+syfos@users.noreply.github.com>
2026-09-09 16:34:04 +0530

    refactor(app): remove pub struct EventManager

--- src/app/mod.rs
+++ src/app/mod.rs
@@ -1,19 +1,15 @@
-use crate::{action::EventManager, ui::Tui, watcher::WatchSignals};
+use crate::{ui::Tui, watcher::WatchSignals};
 use std::sync::Arc;
 
-#[allow(dead_code)]
 pub struct App {
   pub tui: Tui,
-  pub event_manager: EventManager,
   pub watcher_signal: Arc<WatchSignals>,
 }
 
-#[allow(dead_code)]
 impl App {
   pub fn new() -> Result<App, Box<dyn std::error::Error>> {
     Ok(App {
       tui: Tui::new()?,
-      event_manager: EventManager::default(),
       watcher_signal: WatchSignals::spawn()?,
     })
   }
