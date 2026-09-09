12e71e913efc1198696160a60c6af88b07702a64
syfos <261843105+syfos@users.noreply.github.com>
2026-09-09 15:35:35 +0530

    fix(ui): use Editor::enable_modal_keymaps in renderer.rs

--- src/ui/renderer.rs
+++ src/ui/renderer.rs
@@ -20,18 +20,10 @@ impl Tui {
         self.screen_area = frame.area();
         Splits::render(&mut self.splits, frame);
         Editor::render_rope(&mut self.editor, frame, self.screen_area);
-
-        // buffer_mut gives mut ref of buffer
-        // [(self.cursor_col, self.cursor_row)] means access the given cell.
-        // Note that every (cursor_col, cursor_row) is a cell
-        // frame.buffer_mut()[(self.editor.cursor.0 as u16, self.editor.cursor.1 as u16)]
-        //   .set_style(Style::default().bg(Color::White).fg(Color::Black));
       })?;
 
-      if self.process_input()? {
+      if self.editor.enable_modal_keymaps()? {
         break Ok(());
-      } else {
-        continue;
       }
     }
   }
