dcfc912a2c45fd457f56870472fb9db71a967cc0
syfos <261843105+syfos@users.noreply.github.com>
2026-09-09 16:35:38 +0530

    refactor(keys): shift pub struct ModeType to editor.rs

--- src/ui/keys/editor.rs
+++ src/ui/keys/editor.rs
@@ -1,9 +1,16 @@
-use crate::{action::ModeType, ui::editor::Editor};
+use crate::ui::editor::Editor;
+
+#[derive(Default, PartialEq)]
+pub enum ModeType {
+  #[default]
+  Normal,
+  Visual,
+  Insert,
+}
 
 // Note: The keymaps are editor(Sycode) specific, that's why I've scoped the function to Editor.
 //
 // Note: The Shell and Fgit will have seperate maps from Sycode.
-#[allow(dead_code)]
 impl Editor {
   pub fn enable_modal_keymaps(&mut self) -> std::io::Result<bool> {
     match self.mode {
