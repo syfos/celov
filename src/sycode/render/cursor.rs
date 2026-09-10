b1b9423ea348bc8c8e2aeef5e24f395faf90356e
syfos <261843105+syfos@users.noreply.github.com>
2026-09-10 16:32:59 +0530

    fix(sycode): change module name from "editor" to "modal" for files using the module

--- src/sycode/render/cursor.rs
+++ src/sycode/render/cursor.rs
@@ -1,7 +1,7 @@
 use std::io::stdout;
 
 use crate::sycode::core::Cursor;
-use crate::sycode::keymaps::editor::ModeType;
+use crate::sycode::keymaps::modal::ModeType;
 use crossterm::cursor::SetCursorStyle;
 use crossterm::execute;
 use ratatui::Frame;
