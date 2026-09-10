65443e9f320086035a251fa8680446ae8e783bb9
syfos <261843105+syfos@users.noreply.github.com>
2026-09-10 15:51:05 +0530

    docs(cursor): add doc comment to Cursor::draw and Cursor::render

--- src/sycode/cursor/render.rs
+++ src/sycode/cursor/render.rs
@@ -8,16 +8,24 @@ use ratatui::Frame;
 
 #[allow(dead_code)]
 impl Cursor {
+  /// Renders native terminal cursor respecting the [`ModeType`].
+  /// Put `is_blinking: false` to stop blinking. 
+  ///
+  /// Note:
+  /// 1. Block for `ModeType::Normal` and `ModeType::Visual`.
+  /// 2. Bar for `ModeType::Insert`.
   pub fn render(
     &mut self,
     mode: &ModeType,
     frame: &mut Frame,
   ) -> Result<(), Box<dyn std::error::Error>> {
     match mode {
+      // Normal and Visual need Block cursor.
       ModeType::Normal | ModeType::Visual => {
         self.draw(frame, SetCursorStyle::BlinkingBlock)?;
       }
 
+      // Insert needs Bar cursor.
       ModeType::Insert => {
         self.draw(frame, SetCursorStyle::BlinkingBar)?;
       }
@@ -25,12 +33,15 @@ impl Cursor {
     Ok(())
   }
 
+  /// Draws terminal's native cursor according to the given style.
   fn draw(
     &mut self,
     frame: &mut Frame,
     cursor_type: SetCursorStyle,
   ) -> Result<(), Box<dyn std::error::Error>> {
+    // Sets the cursor's postion irrespective of buffer.
     frame.set_cursor_position((self.0 as u16, self.1 as u16));
+    // send it to the terminal to process
     execute!(stdout(), cursor_type)?;
     Ok(())
   }
