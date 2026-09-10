6d61009d3a120ecea6a1b5e318faffdb40eca3aa
syfos <261843105+syfos@users.noreply.github.com>
2026-09-10 15:39:14 +0530

    feat(cursor): use terminal's default blinking cursor

--- src/sycode/cursor/render.rs
+++ src/sycode/cursor/render.rs
@@ -1,33 +1,37 @@
+use std::io::stdout;
+
 use crate::sycode::core::Cursor;
 use crate::sycode::keymaps::editor::ModeType;
-use ratatui::style::Color;
-use ratatui::{Frame, style::Style};
+use crossterm::cursor::SetCursorStyle;
+use crossterm::execute;
+use ratatui::Frame;
 
 #[allow(dead_code)]
 impl Cursor {
-  /// Note: This render function must be placed in the Tui::renderer's draw loop for instant update.
-  /// Warn: Don't place it inside key input match block
-  pub fn render(&mut self, mode: &ModeType, frame: &mut Frame) {
+  pub fn render(
+    &mut self,
+    mode: &ModeType,
+    frame: &mut Frame,
+  ) -> Result<(), Box<dyn std::error::Error>> {
     match mode {
       ModeType::Normal | ModeType::Visual => {
-        self.draw_block(frame);
+        self.draw(frame, SetCursorStyle::BlinkingBlock)?;
       }
+
       ModeType::Insert => {
-        self.draw_bar(frame);
+        self.draw(frame, SetCursorStyle::BlinkingBar)?;
       }
     }
+    Ok(())
   }
 
-  fn draw_block(&mut self, frame: &mut Frame) {
-    let buf = frame.buffer_mut();
-    let cell = &mut buf[(self.0 as u16, self.1 as u16)];
-    cell.set_style(Style::default().fg(Color::Black).bg(Color::White));
-  }
-
-  fn draw_bar(&mut self, frame: &mut Frame) {
-    let buf = frame.buffer_mut();
-    let cell = &mut buf[(self.0 as u16, self.1 as u16)];
-    cell.set_symbol("|");
-    cell.set_style(Style::default().fg(Color::White));
+  fn draw(
+    &mut self,
+    frame: &mut Frame,
+    cursor_type: SetCursorStyle,
+  ) -> Result<(), Box<dyn std::error::Error>> {
+    frame.set_cursor_position((self.0 as u16, self.1 as u16));
+    execute!(stdout(), cursor_type)?;
+    Ok(())
   }
 }
