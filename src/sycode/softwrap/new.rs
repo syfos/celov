3304653fce38736b616a18922f2e59251d03cfe6
syfos <261843105+syfos@users.noreply.github.com>
2026-09-11 19:02:07 +0530

    feat(softwrap): add Wrap::get_processed_line

--- src/sycode/softwrap/new.rs
+++ src/sycode/softwrap/new.rs
@@ -1,7 +1,35 @@
+use ropey::Rope;
+
+use crate::sycode::unicode::icu_engines::IcuEngines;
+
 pub struct Wrap;
 #[allow(dead_code)]
 pub struct ProcessedLine {
   pub line_idx: usize,
   pub line_to_byte: usize,
   pub slices: Vec<String>,
+}
+
+#[allow(dead_code)]
+impl Wrap {
+  /// Returns vector of byte indicies which have a valid break opportunity.
+  /// Note: The byte indices are absolute byte indices of the given rope line.
+  fn get_processed_line(icu: &IcuEngines, rope: &Rope, line_idx: usize) -> ProcessedLine {
+    let line = rope.line(line_idx).to_string();
+    let line_to_byte = rope.line_to_byte(line_idx);
+    let breakpoints: Vec<usize> = icu.line.segment_str(&line).collect();
+
+    let mut slices = Vec::new();
+
+    for window in breakpoints.windows(2) {
+      let slice = line[window[0]..window[1]].to_string();
+      slices.push(slice);
+    }
+
+    ProcessedLine {
+      line_idx,
+      line_to_byte,
+      slices,
+    }
+  }
 }
