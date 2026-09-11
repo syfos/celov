43d1f875d5b4e37b7ac23f7c031a159014202ed2
syfos <261843105+syfos@users.noreply.github.com>
2026-09-11 07:12:35 +0530

    refactor(cursor): delete pub struct Cursor

--- src/sycode/cursor/cursor_struct.rs
+++ src/sycode/cursor/cursor_struct.rs
@@ -1,7 +1,3 @@
-/// The cursor position in `(col, row)` format.
-#[derive(Default, Clone, Copy)]
-pub struct Cursor(pub usize, pub usize);
-
 /// This temporary data sturct is ment to reduce chaos ans allow programming.
 /// Note: Ratatui accepts (col, row) instead of (row, col).
 #[derive(Default, Clone, Copy)]
