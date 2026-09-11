59ab02c609eafe6aa66b10f7b7c07e132a0cb582
syfos <261843105+syfos@users.noreply.github.com>
2026-09-11 09:22:45 +0530

    docs(curosr_struct): correct spelling "row" to "rows"

--- src/sycode/cursor/cursor_struct.rs
+++ src/sycode/cursor/cursor_struct.rs
@@ -4,7 +4,7 @@
 pub struct Cursor {
   /// Postition of cursor in column.
   pub col_pos: usize,
-  /// Position of cursor in row.
+  /// Position of cursor in rows.
   pub row_pos: usize,
   /// The rope line index the cursor is standing on.
   pub current_line_idx: usize,
