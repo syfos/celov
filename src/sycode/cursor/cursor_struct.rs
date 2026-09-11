7304b2d72592355c1bb10130f0ad3c0e0921db17
syfos <261843105+syfos@users.noreply.github.com>
2026-09-11 09:11:29 +0530

    docs(cursor_struct): add doc comment to fields of Cursor

--- src/sycode/cursor/cursor_struct.rs
+++ src/sycode/cursor/cursor_struct.rs
@@ -2,6 +2,12 @@
 /// Note: Ratatui accepts cordinates in `(col, row)` format instead of `(row, col)`.
 #[derive(Default, Clone, Copy)]
 pub struct Cursor {
+  /// Postition of cursor in column.
   pub col: usize,
+  /// Position of cursor in row.
   pub row: usize,
+  /// The row cursor is standing on. 
+  pub current_row_idx: usize,
+  /// The rope line index the cursor is standing on.
+  pub current_line_idx: usize,
 }
